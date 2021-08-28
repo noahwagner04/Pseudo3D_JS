b49650efeb4bcf69f4f41dd8766d18d2da9c31cb
Noah Wagner <wagnernoah@icloud.com>
2021-08-28 13:00:38 -0700

    changing structure, got rid of loader namespace

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-var V = new Pseudo3D.Math.Vector(-7, -4);
-var V2 = new Pseudo3D.Math.Vector(17, 6.5);
-var C = new Pseudo3D.Resources.Color();
-var I = new Pseudo3D.Resources.ImageContainer("penis", [255, 34, 23]);
-var SS = new Pseudo3D.Resources.SpriteSheet();
-var CAM = new Pseudo3D.Camera({x: undefined, y: undefined});
-var E = new Pseudo3D.Entity();
-var R = new Pseudo3D.Ray();
-var REN = new Pseudo3D.Renderer();
-var SC = new Pseudo3D.Scene();
-var S = new Pseudo3D.Sprite({x: undefined, y: undefined});
-console.log(V,C,I,SS,CAM,R,REN,SC,S);
+Pseudo3D.Loader.loadColor("barrelColor", [1, 2, 3, 100], "hsb");
+Pseudo3D.Loader.loadImage("barrel", "/pics/barrel.png", "barrelColor");
+Pseudo3D.Loader.loadImage("bluestone", "/pics/bluestone.png", "d");
+Pseudo3D.Loader.loadImage("colorstone", "/pics/colorstone.png", [23, 3]);
+Pseudo3D.Loader.loadImage("eagle", "/pics/eagle.png", [2344, 54, 45, 455]);
+var barrel = Pseudo3D.Loader.getResource("barrel", (img) => printImage(img));
+var bluestone = Pseudo3D.Loader.getResource("bluestone", (img) => printImage(img));
+var colorstone = Pseudo3D.Loader.getResource("colorstone", (img) => printImage(img));
+var eagle = Pseudo3D.Loader.getResource("eagle", (img) => printImage(img));
+
+// var barrelColor = new Pseudo3D.Color([1, 2, 3, 100]);
+// var barrel = new Pseudo3D.Image("/pics/barrel.png", [65, 26, 6, 100]);
+// var bluestone = new Pseudo3D.Image("/pics/bluestone.png", barrelColor);
+// var colorstone = new Pseudo3D.Image("/pics/colorstone.png");
+// var eagle = new Pseudo3D.Image("/pics/eagle.png" [23, 43, 5]);
+
+function printImage(img) {
+	console.log(img);
+}
