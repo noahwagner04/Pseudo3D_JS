67f089c441fd26d40e333d3fe24e830a520a488b
Noah Wagner <wagnernoah@icloud.com>
2021-08-29 21:42:35 -0700

    got rid of loader class, making resources handle their own loading

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-Pseudo3D.Loader.loadColor("barrelColor", [1, 2, 3, 100], "hsb");
-Pseudo3D.Loader.loadImage("barrel", "/pics/barrel.png", "barrelColor");
-Pseudo3D.Loader.loadImage("bluestone", "/pics/bluestone.png", "d");
-Pseudo3D.Loader.loadImage("colorstone", "/pics/colorstone.png", [23, 3]);
-Pseudo3D.Loader.loadImage("eagle", "/pics/eagle.png", [2344, 54, 45, 455]);
-var barrel = Pseudo3D.Loader.getResource("barrel", (img) => printImage(img));
-var bluestone = Pseudo3D.Loader.getResource("bluestone", (img) => printImage(img));
-var colorstone = Pseudo3D.Loader.getResource("colorstone", (img) => printImage(img));
-var eagle = Pseudo3D.Loader.getResource("eagle", (img) => printImage(img));
+// Pseudo3D.Loader.loadColor("barrelColor", [1, 2, 3, 100], "hsb");
+// Pseudo3D.Loader.loadImage("barrel", "/pics/barrel.png", "barrelColor");
+// Pseudo3D.Loader.loadImage("bluestone", "/pics/bluestone.png", "d");
+// Pseudo3D.Loader.loadImage("colorstone", "/pics/colorstone.png", [23, 3]);
+// Pseudo3D.Loader.loadImage("eagle", "/pics/eagle.png", [2344, 54, 45, 455]);
+// var barrel = Pseudo3D.Loader.getResource("barrel", (img) => printImage(img));
+// var bluestone = Pseudo3D.Loader.getResource("bluestone", (img) => printImage(img));
+// var colorstone = Pseudo3D.Loader.getResource("colorstone", (img) => printImage(img));
+// var eagle = Pseudo3D.Loader.getResource("eagle", (img) => printImage(img));
 
 // var barrelColor = new Pseudo3D.Color([1, 2, 3, 100]);
 // var barrel = new Pseudo3D.Image("/pics/barrel.png", [65, 26, 6, 100]);
