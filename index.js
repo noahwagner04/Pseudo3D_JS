dfa36e3bb5f6738ea915e45eb95883ee0723fa14
Noah Wagner <wagnernoah@icloud.com>
2021-09-07 11:03:58 -0700

    finishing up resource types

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-// Pseudo3D.Loader.loadColor("barrelColor", [1, 2, 3, 100], "hsb");
-// Pseudo3D.Loader.loadImage("barrel", "/pics/barrel.png", "barrelColor");
-// Pseudo3D.Loader.loadImage("bluestone", "/pics/bluestone.png", "d");
-// Pseudo3D.Loader.loadImage("colorstone", "/pics/colorstone.png", [23, 3]);
-// Pseudo3D.Loader.loadImage("eagle", "/pics/eagle.png", [2344, 54, 45, 455]);
-// var barrel = Pseudo3D.Loader.getResource("barrel", (img) => printImage(img));
-// var bluestone = Pseudo3D.Loader.getResource("bluestone", (img) => printImage(img));
-// var colorstone = Pseudo3D.Loader.getResource("colorstone", (img) => printImage(img));
-// var eagle = Pseudo3D.Loader.getResource("eagle", (img) => printImage(img));
+var barrelColor = new Pseudo3D.Color([1, 2, 3, 100]);
+var barrel = new Pseudo3D.SpriteSheet("/pics/eagle.png", [65, 26, 6, 100, "hsb"], 32, 32);
+var bluestone = new Pseudo3D.Texture("/pics/bluestone.png", barrelColor);
+var colorstone = new Pseudo3D.Texture("/pics/colorstone.png");
+var eagle = new Pseudo3D.Texture("/pics/eagle.png", [23, 43, 5]);
+barrel.source.html.onload = test;
+console.log(barrel);
+console.log(bluestone);
+console.log(colorstone);
+console.log(eagle);
 
-// var barrelColor = new Pseudo3D.Color([1, 2, 3, 100]);
-// var barrel = new Pseudo3D.Image("/pics/barrel.png", [65, 26, 6, 100]);
-// var bluestone = new Pseudo3D.Image("/pics/bluestone.png", barrelColor);
-// var colorstone = new Pseudo3D.Image("/pics/colorstone.png");
-// var eagle = new Pseudo3D.Image("/pics/eagle.png" [23, 43, 5]);
-
-function printImage(img) {
-	console.log(img);
+function test() {
+	var c = document.createElement("canvas");
+	var ctx = c.getContext("2d");
+	document.body.appendChild(c);
+	let x = 0;
+	let y = 0;
+    barrel.textures.forEach(texture => {
+    	var imageData = new ImageData(texture.pixels, barrel.cellWidth);
+    	if (y >= barrel.source.height) {
+    		y = 0;
+    		x += barrel.cellWidth;
+    	}
+    	ctx.putImageData(imageData, x, y);
+    	y += barrel.cellHeight;
+    });
 }
+
