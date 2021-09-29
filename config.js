bcf5cbadb63a22ac86da0fec1c9c01b94aa4fa31
Noah Wagner <wagnernoah@icloud.com>
2021-09-28 17:41:49 -0700

    added heigher walls, camera pitch, different camera z position, and better movement

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var blueStoneColor = new Pseudo3D.Color([20, 20, 150]);
 var bluestone = new Pseudo3D.Texture("/pics/bluestone.png", blueStoneColor);
 var colorstone = new Pseudo3D.Texture("/pics/colorstone.png", [100, 100, 100]);
 var eagle = new Pseudo3D.Texture("/pics/eagle.png", [168, 0, 0], 64, 64);
-var sky = new Pseudo3D.Texture("/pics/skybox2.png", [109, 210, 255]);
+var sky = new Pseudo3D.Texture("/pics/skybox2.png", [109, 210, 255], 512, 480);
 var barrel = new Pseudo3D.Texture("/pics/barrel.png", [65, 26, 6]);
 var wood = new Pseudo3D.Texture("/pics/wood.png", [110, 74, 29]);
 
@@ -54,13 +54,17 @@ config.worldMap = [
 
 config.cellPrefabs = {
 	1: {
-		texture: colorstone
+		texture: colorstone,
+		start: 0,
+		end: 1
 	},
 	3: {
-		texture: eagle
+		texture: eagle,
+		end: 1
 	},
 	7: {
-		texture: wood
+		texture: wood,
+		end: 0.3
 	}
 };
 
--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 var scene = new Pseudo3D.Scene(config);
-var renderer = new Pseudo3D.Renderer(900, 600, 0.666);
+var renderer = new Pseudo3D.Renderer(900, 600, 0.444);
 document.body.appendChild(renderer.canvas);
 renderer.canvas.style.border = "8px solid rgb(210, 210, 210)";
 var camera = new Pseudo3D.Camera({
 	type: Pseudo3D.RenderTypes.RAY,
 	farClippingPlane: 12,
-	nearClippingPlane: 0,
+	nearClippingPlane: 0.2,
 	planeLength: 0.75,
 	x: 10,
 	y: 12,
 	spriteSettings: {
 		nearClippingPlane: 0.2,
-		farClippingPlane: 3,
+		farClippingPlane: 7,
 		partialAlpha: false
 	},
 });
+
+renderer.canvas.requestPointerLock = renderer.canvas.requestPointerLock ||
+	renderer.canvas.mozRequestPointerLock;
+
+renderer.canvas.onclick = function() {
+	renderer.canvas.requestPointerLock();
+}
 
 function test(amount) {
 	for (var i = 0; i < amount; i++) {
@@ -25,16 +32,16 @@ function test(amount) {
 var stop = false;
 var frameCount = 0;
 var fps = 60, // can only be a factor of 60 when below 30, and when above 30 can only be 60 - fps factor of 60 to work.
-	frameCount = 0, 
-	skipFrames, 
-	frameRate = 0, 
-	f = 0, 
+	frameCount = 0,
+	skipFrames,
+	frameRate = 0,
+	f = 0,
 	fc = 0;
 // list of valid values for fps are: 1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 40, 45, 48, 50, 54, 55, 56, 57, 58, 59, 60
 
-if(fps > 30) {
+if (fps > 30) {
 	skipFrames = 60 / (60 - fps);
-} else if(fps <= 30){
+} else if (fps <= 30) {
 	skipFrames = 60 / fps;
 }
 
@@ -54,10 +61,10 @@ function animate() {
 	if ((fps >= 30 && frameCount % skipFrames !== 0) || (fps < 30 && frameCount % skipFrames === 0)) {
 		// if(frameCount === 0) stop = true
 		if (keysDown["a"]) {
-			camera.rotate(270 / fps);
+			camera.position.subtract(camera.plane.clone().scale(4 / fps));
 		}
 		if (keysDown["d"]) {
-			camera.rotate(-270 / fps);
+			camera.position.add(camera.plane.clone().scale(4 / fps));
 		}
 		if (keysDown["w"]) {
 			camera.position.add(camera.direction.clone().scale(4 / fps));
@@ -80,18 +87,19 @@ function animate() {
 		// 	console.log("low frameRate");
 		// } 
 		// else {
-			renderer.drawingContext.fillRect(0, 0, 900, 600);
-			renderer.render(scene, camera);
+		renderer.drawingContext.fillRect(0, 0, 900, 600);
+		renderer.render(scene, camera);
 		// }
+		camera.position.z = 0.03 * Math.sin(frameCount / 6) + 0.5;
 		fc++;
 	}
 	frameCount++;
 }
 
-setInterval(() => {
-	console.log(fc - f);
-	f = fc;
-}, 1000);
+// setInterval(() => {
+// 	console.log(fc - f);
+// 	f = fc;
+// }, 1000);
 
 var keysDown = {};
 window.addEventListener("keydown", (e) => {
@@ -100,5 +108,15 @@ window.addEventListener("keydown", (e) => {
 
 window.addEventListener("keyup", (e) => {
 	delete keysDown[e.key];
+});
+
+var mouseX = 0;
+var mouseY = 0;
+window.addEventListener("mousemove", (e) => {
+	mouseX += e.movementX;
+	mouseY += e.movementY;
+	var angle = Pseudo3D.Math.remap(mouseX, 0, 900, 180, -180);
+	camera.setRotation(angle);
+	camera.pitch = 300 - mouseY;
 });
 animate();
